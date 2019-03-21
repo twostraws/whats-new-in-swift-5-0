f7e816b3de88a2f619ea10876de5414b3862daf5
Paul Hudson <paul.hudson@gmail.com>
2019-03-21 20:14:38 +0000

    count(where:) has been deferred.

--- Whats-New-In-Swift-5-0.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Whats-New-In-Swift-5-0.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
  # What’s new in Swift 5.0
 
  * Created by [Paul Hudson](https://twitter.com/twostraws) – [Hacking with Swift](https://www.hackingwithswift.com)
- * Last update: January 10th 2019
+ * Last update: March 21st 2019
 
  This playground is designed to showcase new features introduced with Swift 5.0. I already [wrote an article on it](https://www.hackingwithswift.com/articles/126/whats-new-in-swift-5-0), but it's a lot more fun to see things in action and experiment yourself.
 
@@ -21,7 +21,6 @@
  * [Handling future enum cases](Handling%20future%20enum%20cases)
  * [Flattening nested optionals resulting from try?](Flattening%20nested%20optionals%20resulting%20from%20try%3F)
  * [Checking for integer multiples](Checking%20for%20integer%20multiples)
- * [Counting matching items in a sequence](Counting%20matching%20items%20in%20a%20sequence)
  * [Transforming and unwrapping dictionary values with compactMapValues()](Transforming%20and%20unwrapping%20dictionary%20values)
 
  &nbsp;
